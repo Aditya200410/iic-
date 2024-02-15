b34b0a5ec075f49b9c31364be148b22638830ece
Aditya200410 <128316171+Aditya200410@users.noreply.github.com>
2024-02-15 23:09:38 +0530

    final

--- src/Pages/ticket/Ticket.jsx
+++ src/Pages/ticket/Ticket.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useRef } from "react";
 import logo from "./Ticket.png";
 import "./ticket.css";
-
+import Resizer from "react-image-file-resizer";
 const Ticket = () => {
-  
   const [selectedFile, setSelectedFile] = useState(null);
 
   const handleFileChange = (event) => {
@@ -25,6 +24,9 @@ const Ticket = () => {
   };
   const [userName, setUserName] = useState("");
   const [displayedName, setDisplayedName] = useState("");
+  const [selectedImage, setSelectedImage] = useState({
+    image: ""
+  });
   const imageRef = useRef();
 
   const handleInputChange = (event) => {
@@ -33,42 +35,79 @@ const Ticket = () => {
 
   const handleDisplayButtonClick = () => {
     setDisplayedName(userName);
-    if(userName)
-    alert("ticket generated")
-  else{
-    alert(" enter your name")
-  }
   };
+  const resizeFile = (file) =>
+    new Promise((resolve) => {
+      Resizer.imageFileResizer(
+        file,
+        967,
+        307,
+        "JPG",
+        100,
+        0,
+        (uri) => {
+          resolve(uri);
+        },
+        "file"
+      );
+    });
 
   const handleDownloadButtonClick = () => {
     const tempCanvas = document.createElement("canvas");
     const tempContext = tempCanvas.getContext("2d");
-  
+
     const originalImage = new Image();
-    originalImage.src = imageRef.current.src;
-  
+    originalImage.src = imageRef.current.children[0].src;
+    const userImage = new Image();
+    userImage.src = imageRef.current.children[1].src;
+
     originalImage.onload = () => {
       tempCanvas.width = originalImage.width;
       tempCanvas.height = originalImage.height;
-  
+
       tempContext.drawImage(originalImage, 0, 0);
-  
+
       tempContext.fillStyle = "white";
       tempContext.font = "bold 48px sans-serif";
       tempContext.textAlign = "center";
       tempContext.textBaseline = "middle";
-      tempContext.fillText(displayedName, 1100, 475);
-  
+      tempContext.fillText(displayedName, 1150, 310);
+
+      // Save the current context state
+      tempContext.save();
+
+      // Set a clipping region
+      tempContext.beginPath();
+      tempContext.arc(originalImage.width - 1420 + 240, 46 + 240,190,0, Math.PI * 2);
+      tempContext.closePath();
+      tempContext.clip();
+
+      tempContext.drawImage(userImage, originalImage.width - 1420, 46, 480, 480);
+
+      // Restore the previous context state
+      tempContext.restore();
+
       const dataUrl = tempCanvas.toDataURL("image/png");
-  
+
       const link = document.createElement("a");
       link.href = dataUrl;
       link.download = "output_photo.png";
-  
+
       link.click();
     };
-  };  
+  };
 
+
+
+
+  const handleSubmit = () => {
+
+  }
+  const handleProfileImageChange = async (e) => {
+    const file = e.target.files[0];
+    const url = await resizeFile(file);
+    setSelectedImage({ ...selectedImage, image: url });
+  };
   return (
     <>
       <div className="t">
@@ -93,6 +132,12 @@ const Ticket = () => {
                 className="tf"
                 placeholder="enter your mail"
               />
+              <input
+                type="file"
+                name="image"
+                inputProps={{ accept: 'image/*' }}
+                onChange={handleProfileImageChange}
+              />
             </div>
             <div className="t3">
               <div className="t4">
@@ -103,13 +148,23 @@ const Ticket = () => {
               {displayedName && (
                 <div>
                   <div
+                    ref={imageRef}
                     style={{ position: "relative", display: "inline-block" }}
                   >
                     <img
-                      ref={imageRef}
                       src={logo}
                       alt="User Display"
                       style={{ display: "none" }}
+                    />
+                    <img
+                      src={URL.createObjectURL(selectedImage.image)}
+                      alt="Selected Profile"
+                      style={{
+                        display: "none",
+                        objectFit: 'contain',
+                        borderRadius: '50%',
+                        display: "none",
+                      }}
                     />
                   </div>
                   <button className="d1" onClick={handleDownloadButtonClick}>
@@ -120,7 +175,7 @@ const Ticket = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div >
     </>
   );
 };
